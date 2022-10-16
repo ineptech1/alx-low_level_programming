49982ab8f559f36f1d4ff5238d6fee95479b28b4
ineptech1 <ineptechnology@gmail.com>
2022-10-16 10:32:40 -0700

    Standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -program that prints standard error
+ *
+ * Return: 1 on success
+*/
+int main(void)
+{
+	input(2,"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
