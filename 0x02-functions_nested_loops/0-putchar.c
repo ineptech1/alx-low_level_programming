b47831e3e293799a5684d526f6676e62e4ceaf85
ineptech1 <ineptechnology@gmail.com>
2022-10-18 22:01:56 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main -print _putchar
+ *
+ * prints and usage of created libraries
+ * is written by Emoigho Precious Egwofobe
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
