af8d428a22fb36d7b2b5c3cbd6cecb71791064f6
ineptech1 <ineptechnology@gmail.com>
2022-10-19 08:46:02 -0700

    Fibon

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
-
 /**
  *  main - Prints the first 98 Fibonacci numbers
- *
- *  Return: Always 0.
- */
-int main(void)
+*/
 {
 	int c, boolean, boolean2;
 	long int n1, n2, fn, fn2, n11, n22;
-
 	n1 = 1;
 	n2 = 2;
 	boolean =  boolean2 = 1;
@@ -20,8 +15,6 @@ int main(void)
 		{
 			fn = n1 + n2;
 			printf(", %ld", fn);
-			n1 = n2;
-			n2 = fn;
 		}
 		else
 		{
@@ -33,13 +26,9 @@ int main(void)
 				n2 = n2 / 1000000000;
 				boolean2 = 0;
 			}
-			fn2 = (n11 + n22);
 			fn = n1 + n2 + (fn2 / 1000000000);
 			printf(", %ld", fn);
 			printf("%ld", fn2 % 1000000000);
-			n1 = n2;
-			n11 = n22;
-			n2 = fn;
 			n22 = (fn2 % 1000000000);
 		}
 		if (((n1 + n2) < 0) && boolean == 1)
