08a676ed416d802cf126cf6f522cb3eb0f22a790
ineptech1 <ineptechnology@gmail.com>
2022-11-10 19:39:42 -0800

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,30 +9,9 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	char *p;
+	void *precious = malloc(b);
 
-	p = malloc(b);
-	if (p == NULL)
+	if (precious == NULL)
 		exit(98);
-	return (p);
-}
-
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-/**
- * malloc_checked - allocate a space
- * @b: unsigned int
- * Return: void
- */
-void *malloc_checked(unsigned int b)
-{
-void *x;
-
-x = malloc(b);
-if (x == NULL)
-{
-	exit(98);
-}
-return (x);
+	return (precious);
 }
