6b27b3ff06550786c426e6b8d1d0889d59f8450b
ineptech1 <ineptechnology@gmail.com>
2022-11-14 18:36:51 -0800

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - prinnts out a file name and a new line
+  * Return: void
+  */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
