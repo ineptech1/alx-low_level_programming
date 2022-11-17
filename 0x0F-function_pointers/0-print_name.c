d7358a15ade4b56f96ee5455aefb9f42ad131f12
ineptech1 <ineptechnology@gmail.com>
2022-11-16 19:40:11 -0800

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,13 +3,13 @@
 #include <stdio.h>
 
 /**
-  * print_name - Prints a name
-  * @name: input the name
-  * @f: function pointer to name
-  * Return: void.
+  * print_name - Prints a name.
+  * @name: input the name.
+  * @f: function pointer.
+  * Return: nothing.
   */
 
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
 {
 	if (f != NULL)
 		f(name);
