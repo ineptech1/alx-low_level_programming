bb8bd92604509ff8eba7d7a5dabcee07b51ebd86
ineptech1 <ineptechnology@gmail.com>
2022-11-28 18:54:06 -0800

    lists.h

--- 0x13-more_singly_linked_lists/newlist.h
+++ 0x13-more_singly_linked_lists/newlist.h
@@ -1,5 +1,5 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
+#ifndef _LISTS_
+#define _LISTS_
 
 #include <Stdio.h>
 #include <stdlib.h>
@@ -47,4 +47,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#endif /* _LISTS_H_ */
+#endif
