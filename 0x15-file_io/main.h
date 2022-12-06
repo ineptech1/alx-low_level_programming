bd2f6683f2e0ae05ab65f5e9c181a24a740aad5f
ineptech1 <ineptechnology@gmail.com>
2022-12-05 18:43:54 -0800

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,5 @@
-#ifndef _FILE_IO_
-#define _FILE_IO_
+#ifndef _MAIN_
+#define _MAIN_
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -11,4 +11,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif /* FILE_IO */
+#endif /* MAIN */
