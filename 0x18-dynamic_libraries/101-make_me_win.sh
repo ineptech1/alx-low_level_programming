e9640340bd3673b7fb65c46d63a49890abc87051
ineptech1 <ineptechnology@gmail.com>
2023-01-16 21:14:14 -0800

    Advanced

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 wget -P /tmp https://github.com/ineptech1/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+d52e6c18e0723f5b025a75dea19ef365  gm
 export LD_PRELOAD=/tmp/iwin.so
